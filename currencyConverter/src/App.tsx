f2231b54802436aca15a685cbd653f2afebb5054
rahul4019 <rahulkashyap4019@gmail.com>
2025-01-15 09:07:29 +0530

    Initialize currencyConverter app and define currency interface

---
+++ currencyConverter/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+function App(): React.JSX.Element {
+  return <Text>Hello world</Text>;
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
