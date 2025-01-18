e7850e2327ee268513adb150d14edadf73dcf7e5
rahul4019 <rahulkashyap4019@gmail.com>
2025-01-18 21:43:22 +0530

    Add app style

--- ticTacToe/src/App.tsx
+++ ticTacToe/src/App.tsx
@@ -61,10 +61,33 @@ function App(): React.JSX.Element {
       setGameWinner(`Player ${board[0][2]} has won the game! 🥳`);
     }
 
-    const isDraw = board.map(arr => arr.includes(null));
+    const isDraw = board.flat().every(cell => cell !== null);
     if (isDraw) {
       setGameWinner('Its a Draw...⌛');
     }
+  };
+
+  const onChangeItem = (rowNum: number, colNum: number) => {
+    if (gameWinner) {
+      return Snackbar.show({
+        text: gameWinner,
+        backgroundColor: '#000000',
+        textColor: '#ffffff',
+      });
+    }
+
+    if (board[rowNum][colNum] === null) {
+      board[rowNum][colNum] = isCross ? 'cross' : 'circle';
+      setIsCross(!isCross);
+    } else {
+      return Snackbar.show({
+        text: 'Position is already filled',
+        backgroundColor: 'red',
+        textColor: '#ffffff',
+      });
+    }
+
+    isWinner();
   };
 
   return (
@@ -77,6 +100,75 @@ function App(): React.JSX.Element {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  playerInfo: {
+    height: 56,
 
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginVertical: 12,
+    marginHorizontal: 14,
+
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.2,
+    shadowRadius: 1.5,
+  },
+  gameTurnTxt: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+  },
+  playerX: {
+    backgroundColor: '#38CC77',
+  },
+  playerO: {
+    backgroundColor: '#F7CD2E',
+  },
+  grid: {
+    margin: 12,
+  },
+  card: {
+    height: 100,
+    width: '33.33%',
+
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    borderWidth: 1,
+    borderColor: '#333',
+  },
+  winnerInfo: {
+    borderRadius: 8,
+    backgroundColor: '#38CC77',
+
+    shadowOpacity: 0.1,
+  },
+  winnerTxt: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+    textTransform: 'capitalize',
+  },
+  gameBtn: {
+    alignItems: 'center',
+
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 36,
+    backgroundColor: '#8D3DAF',
+  },
+  gameBtnText: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    fontWeight: '500',
+  },
+});
 export default App;
