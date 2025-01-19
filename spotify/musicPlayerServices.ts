398bf0d7fa369f5db30e47083313ee63b2b937e4
rahul4019 <rahulkashyap4019@gmail.com>
2025-01-19 09:37:32 +0530

    Configure musicPlayerService with playListData

--- spotify/musicPlayerServices.ts
+++ spotify/musicPlayerServices.ts
@@ -1,6 +1,24 @@
-import TrackPlayer, {Event} from 'react-native-track-player';
+import TrackPlayer, {Event, RepeatMode} from 'react-native-track-player';
 
 import {playlistData} from './src/constants';
+
+export async function setupPlayer() {
+  let isSetup = false;
+  try {
+    await TrackPlayer.getCurrentTrack();
+    isSetup = true;
+  } catch (error) {
+    await TrackPlayer.setupPlayer();
+    isSetup = true;
+  } finally {
+    return isSetup;
+  }
+}
+
+export async function addTrack() {
+  await TrackPlayer.add(playlistData);
+  await TrackPlayer.setRepeatMode(RepeatMode.Queue); // if playlist ends, will start the first one again
+}
 
 export async function playbackService() {
   TrackPlayer.addEventListener(Event.RemotePause, () => {
