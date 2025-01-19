b7296454a37180125d312f5e07d6ec5300b202cd
rahul4019 <rahulkashyap4019@gmail.com>
2025-01-19 09:28:47 +0530

    Create musicPlayer Service and configure music control

---
+++ spotify/musicPlayerServices.ts
@@ -0,0 +1,21 @@
+import TrackPlayer, {Event} from 'react-native-track-player';
+
+import {playlistData} from './src/constants';
+
+export async function playbackService() {
+  TrackPlayer.addEventListener(Event.RemotePause, () => {
+    TrackPlayer.pause();
+  });
+
+  TrackPlayer.addEventListener(Event.RemotePlay, () => {
+    TrackPlayer.play();
+  });
+
+  TrackPlayer.addEventListener(Event.RemoteNext, () => {
+    TrackPlayer.skipToNext();
+  });
+
+  TrackPlayer.addEventListener(Event.RemotePrevious, () => {
+    TrackPlayer.skipToPrevious();
+  });
+}
---
+++ spotify/src/constants.ts
@@ -0,0 +1,3 @@
+import {Track} from 'react-native-track-player'
+
+export const playlistData: Track[] = []
