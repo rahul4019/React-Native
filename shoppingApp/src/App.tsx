b8f89172e78941d4821d7e81d8a607eebb4c0577
rahul4019 <rahulkashyap4019@gmail.com>
2025-01-29 22:59:49 +0530

    Add two routes Home and Details

--- shoppingApp/src/App.tsx
+++ shoppingApp/src/App.tsx
@@ -1,27 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
-// navigation 
-import{NavigationContainer} from '@react-navigation/native'
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+// navigation
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 // screens
-import Home from './screens/Home'
-import Details from './screens/Details'
+import Home from './screens/Home';
+import Details from './screens/Details';
 
 export type RootStackParamList = {
-  Home: undefined,
+  Home: undefined;
   Details: {
-    product: Product
-  }
-}
+    product: Product;
+  };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'Trending Products',
+          }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{
+            title: 'Product Details',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
