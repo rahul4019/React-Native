f02549885786b8edcbbb58b38d9e9284be25dc61
rahul4019 <rahulkashyap4019@gmail.com>
2025-01-29 22:52:08 +0530

    Initialize shopping app with navigation

---
+++ shoppingApp/src/App.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+// navigation 
+import{NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+export default function App() {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
