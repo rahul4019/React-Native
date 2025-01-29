c6f08d684a73b034906f49ba28b06799d6a157d5
rahul4019 <rahulkashyap4019@gmail.com>
2025-01-29 22:56:24 +0530

    Add product interface

--- shoppingApp/src/App.tsx
+++ shoppingApp/src/App.tsx
@@ -5,6 +5,17 @@ import React from 'react'
 import{NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
+// screens
+import Home from './screens/Home'
+import Details from './screens/Details'
+
+export type RootStackParamList = {
+  Home: undefined,
+  Details: {
+    product: Product
+  }
+}
+
 export default function App() {
   return (
     <View>
